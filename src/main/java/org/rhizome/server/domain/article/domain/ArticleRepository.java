57520f85ee868f86b307b7d23b9ae168d792e7eb
EunHyunsu <ehs208@naver.com>
2025-05-09 20:22:06 +0900

    [#3] refactor: 기본 내장된 불필요 JPA 네이밍 쿼리 제거

--- src/main/java/org/rhizome/server/domain/article/domain/ArticleRepository.java
+++ src/main/java/org/rhizome/server/domain/article/domain/ArticleRepository.java
@@ -1,11 +1,9 @@
 package org.rhizome.server.domain.article.domain;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Optional<Article> findById(Long id);
+
 }
