a6dad75889c90b142ff1b31655eca8c2107723f5
EunHyunsu <ehs208@naver.com>
2025-05-09 20:24:11 +0900

    [#3] refactor: 의존성 주입 변경

--- src/main/java/org/rhizome/server/domain/article/controller/ArticleController.java
+++ src/main/java/org/rhizome/server/domain/article/controller/ArticleController.java
@@ -1,6 +1,6 @@
 package org.rhizome.server.domain.article.controller;
 
-import org.rhizome.server.domain.article.service.ArticleServiceImpl;
+import org.rhizome.server.domain.article.service.ArticleService;
 import org.rhizome.server.support.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/articles")
 public class ArticleController {
 
-    private final ArticleServiceImpl articleServiceImpl;
-
-    public ArticleController(ArticleServiceImpl articleServiceImpl) {
-        this.articleServiceImpl = articleServiceImpl;
-    }
+    private final ArticleService articleService;
 
     @GetMapping("/{id}")
     @Operation(summary = "게시글 조회", description = "ID로 게시글을 조회합니다.")
     public ResponseEntity<ApiResponse<?>> getArticle(@PathVariable Long id) {
 
-        return ResponseEntity.ok(ApiResponse.success(articleServiceImpl.getArticle(id)));
+        return ResponseEntity.ok(ApiResponse.success(articleService.getArticle(id)));
     }
 }
