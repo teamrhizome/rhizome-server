a5467d24278cca0391a84cbc2a0273eeb1dd64f8
NaMinhyeok <nmh9097@gmail.com>
2025-05-16 00:46:54 +0900

    feat: docker compose에 nginx 추가

--- src/main/java/org/rhizome/server/config/WebConfig.java
+++ src/main/java/org/rhizome/server/config/WebConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration(proxyBeanMethods = false)
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-			.allowedOrigins("http://localhost:3000", "https://rhizome.kro.kr") // 허용할 도메인
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-			.allowedHeaders("*") // 허용할 헤더
-			.allowCredentials(true); // 인증 정보 허용 여부
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://localhost:3000", "https://rhizome.kro.kr") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 인증 정보 허용 여부
+    }
 }
