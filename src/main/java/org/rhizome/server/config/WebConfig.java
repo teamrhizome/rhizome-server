c070a09d4887b98524ec2bce1d8ca34a4f340056
EunHyunsu <ehs208@naver.com>
2025-05-16 00:00:47 +0900

    fix: preflight를 위한 options 메소드 허용

--- src/main/java/org/rhizome/server/config/WebConfig.java
+++ src/main/java/org/rhizome/server/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
 			.allowedOrigins("http://localhost:3000", "https://api.rhizome.kro.kr") // 허용할 도메인
-			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 허용할 헤더
 			.allowCredentials(true); // 인증 정보 허용 여부
 	}
