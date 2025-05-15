51d1f00058d5b728c8a19914ff97401c677675a4
EunHyunsu <ehs208@naver.com>
2025-05-16 00:10:58 +0900

    fix: cors uri 수정

--- src/main/java/org/rhizome/server/config/WebConfig.java
+++ src/main/java/org/rhizome/server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-			.allowedOrigins("http://localhost:3000", "https://api.rhizome.kro.kr") // 허용할 도메인
+			.allowedOrigins("http://localhost:3000", "https://rhizome.kro.kr") // 허용할 도메인
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 허용할 헤더
 			.allowCredentials(true); // 인증 정보 허용 여부
