e60ec22dc295488cd3281ffdac6df6e7cfd0e176
NaMinhyeok <nmh9097@gmail.com>
2025-05-20 23:12:28 +0900

    [#14] refactor: 수정시 id를 pathvalue로 받는다

--- src/main/java/org/rhizome/server/domain/article/dto/request/UpdateArticleRequest.java
+++ src/main/java/org/rhizome/server/domain/article/dto/request/UpdateArticleRequest.java
@@ -1,3 +1,3 @@
 package org.rhizome.server.domain.article.dto.request;
 
-public record UpdateArticleRequest(Long id, String title, String content, RelateArticleRequest relateArticleIds) {}
+public record UpdateArticleRequest(String title, String content, RelateArticleRequest relateArticleIds) {}
